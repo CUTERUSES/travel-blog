11e7e79074d7ee82396dff99908d434175c98ae6
jenThorn <cjenniferthorn@gmail.com>
2015-05-28 17:11:50 -0400

    fixed loading error with masonry plugin

--- themes/theme-hackeryou/js/plugins.js
+++ themes/theme-hackeryou/js/plugins.js
@@ -1,10 +1,2 @@
 //load in jQuery plugin dependencies (eg. flexslider, smoothScroll etc.) in this file
 
-var container = document.querySelector('#homeContainer');
-var msnry;
-// initialize Masonry after all images have loaded
-imagesLoaded( container, function() {
-  msnry = new Masonry( container, {
-  	itemSelector: 'article.post'
-  });
-});
